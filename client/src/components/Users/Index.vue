8b41328f01b40a9062b7a88a5e95a8850e8157fd
Payday Man <cellgod1993@gmail.com>
2019-06-20 22:33:32 +0700

    V1.04 Vue Component

--- client/src/components/Users/Index.vue
+++ client/src/components/Users/Index.vue
@@ -1,17 +1,29 @@
 <template>
     <div>
         <h1>Get All Users</h1>
+        <div v-if="users.length">
+            <div>User Count {{users.length}}</div>
+            <div>ID: {{users[0].id}}</div>
+            <div>Name-Lastname: {{users[0].name}} - {{users[0].lastname}}</div>
+            <div>email: {{ users[0].email}}</div>
+            <div>password: {{ users[0].password}} </div>
+        </div>
     </div>
 </template>
 
 <script>
+import UsersService from '@/services/UsersService'
+
 export default {
-    created() {
-        this.$http.get('http://localhost:8081/users')
-        .then(function (response) {
-            console.log(response)
-        })
+    data () {
+        return {
+            users:[]
+        }
+    },
+    async created () {
+        this.users = (await UsersService.index()).data
     }
+
 }
 </script>
 
