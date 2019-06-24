2c8c491aa4427aa408aee7b7243a183718f6f93b
Payday Man <cellgod1993@gmail.com>
2019-06-24 17:39:42 +0700

    V1.07 Lession 9 Token part 1

---
+++ server/src/controllers/UserAuthenController.js
@@ -0,0 +1,11 @@
+const {User} = require('../models')
+const config = require('../config/config')
+const jwt = require('jsonwebtoken')
+
+function jwtSignUser(user) {
+    const ONE_WEEK = 60 * 60 * 24 * 7
+    return jwt.sign(user, config.authentication.jwtSecret, {
+        expiresIn: ONE_WEEK
+    })
+    
+}
