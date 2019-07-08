1b1aee38ab949ba81aef23e509390caf0e5dff7a
Payday Man <cellgod1993@gmail.com>
2019-07-08 10:31:02 +0700

    fix

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,36 +1,66 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
-//User
+// Users
 import UserIndex from '@/components/Users/Index'
 import UserCreate from '@/components/Users/CreateUser'
 import UserEdit from '@/components/Users/EditUser'
 import UserShow from '@/components/Users/ShowUser'
-//Authen
+// Authen
 import Login from '@/components/Login'
 
-//Comments
+// Comments
 import CommentIndex from '@/components/Comments/Index'
 
-//Blogs
+// Blogs
 import BlogIndex from '@/components/Blogs/Index'
 import BlogCreate from '@/components/Blogs/CreateBlog'
 import BlogEdit from '@/components/Blogs/EditBlog'
 import BlogShow from '@/components/Blogs/ShowBlog'
+
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
   routes: [
+    // blogs
     {
-      //authen
-        path: '/login',
-        name: 'login',
-        component: Login
-      },
-      //user
-      {
+      path: '/blogs',
+      name: 'blogs',
+      component: BlogIndex
+    },
+    {
+      path: '/blog/create',
+      name: 'blogs-edit',
+      component: BlogCreate
+    },
+    {
+      path: '/blog/edit/:blogId',
+      name: 'blog-edit',
+      component: BlogEdit
+    },
+    {
+      path: '/blog/:blogId',
+      name: 'blog',
+      component: BlogShow
+    },
+
+    // comments
+    {
+      path: '/comments',
+      name: 'comments',
+      component: CommentIndex
+    },
+
+
+    // authen
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    // user
+    {
       path: '/users',
       name: 'users',
       component: UserIndex
@@ -49,37 +79,6 @@ export default new Router({
       path: '/user/:userId',
       name: 'user',
       component: UserShow
-    },
-    
-
-    //Blog
-    {
-      path: '/blogs',
-      name: 'blog-edit',
-      component: BlogIndex
-    },
-    {
-      path: '/blog/create',
-      name: 'blogs-edit',
-      component: BlogCreate
-    },
-    { 
-      path: '/blog/edit/:blogId',
-      name: 'blogs-edit',
-      component: BlogEdit
-    },
-    {
-      path: '/blog/:blogId',
-      name: 'blog',
-      component: BlogShow
-    },
-    
-
-    //Comments
-    {
-      path: '/comments',
-      name: 'comments',
-      component: CommentIndex
-    }
+    },    
   ]
 })
